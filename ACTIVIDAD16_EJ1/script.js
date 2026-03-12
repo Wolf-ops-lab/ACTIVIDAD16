ea120ea4ddf773c65fb75f2b4a5f95facbe73340
Brandon Estrada <branestra44@gmail.com>
2026-03-12 23:41:53 +0000

    Ejercicio 1 finalizado, con validacion

--- ACTIVIDAD16_EJ1/script.js
+++ ACTIVIDAD16_EJ1/script.js
@@ -1,10 +1,11 @@
 const form = document.getElementById('form')
 const username = document.getElementById('username')
+const age = document.getElementById('age')
+const url = document.getElementById('url')
 const email = document.getElementById('email')
 const password = document.getElementById('password')
 const confirmPassword = document.getElementById('confirmpassword')
 
-// Evento submit
 form.addEventListener('submit', function(e){
     e.preventDefault()
     checkInputs()
@@ -13,10 +14,13 @@ form.addEventListener('submit', function(e){
 function checkInputs(){
 
     const usernameValue = username.value.trim()
+    const ageValue = age.value.trim()
+    const urlValue = url.value.trim()
     const emailValue = email.value.trim()
     const passwordValue = password.value.trim()
     const confirmPasswordValue = confirmPassword.value.trim()
 
+    // USERNAME
     if(usernameValue === ''){
         setError(username, "Username is required")
     } else if(usernameValue.length < 3){
@@ -25,6 +29,25 @@ function checkInputs(){
         setSuccess(username)
     }
 
+    // AGE
+    if(ageValue === ''){
+        setError(age, "Age is required")
+    } else if(ageValue < 0 || ageValue > 999){
+        setError(age, "Age must be between 0 and 999")
+    } else {
+        setSuccess(age)
+    }
+
+    // URL
+    if(urlValue === ''){
+        setError(url, "Url is required")
+    } else if(!isURL(urlValue)){
+        setError(url, "Url is not valid")
+    } else {
+        setSuccess(url)
+    }
+
+    // EMAIL
     if(emailValue === ''){
         setError(email, "Email is required")
     } else if(!isEmail(emailValue)){
@@ -33,6 +56,7 @@ function checkInputs(){
         setSuccess(email)
     }
 
+    // PASSWORD
     if(passwordValue === ''){
         setError(password, "Password is required")
     } else if(passwordValue.length < 6){
@@ -41,6 +65,7 @@ function checkInputs(){
         setSuccess(password)
     }
 
+    // CONFIRM PASSWORD
     if(confirmPasswordValue === ''){
         setError(confirmPassword, "Confirm password is required")
     } else if(confirmPasswordValue !== passwordValue){
@@ -67,6 +92,13 @@ function setSuccess(input){
 }
 
 function isEmail(email){
+
     const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
     return re.test(email)
+}
+
+function isURL(url){
+
+    const pattern = /^(https?:\/\/)?([\w\-])+\.{1}([a-zA-Z]{2,63})([\/\w\.-]*)*\/?$/
+    return pattern.test(url)
 }
